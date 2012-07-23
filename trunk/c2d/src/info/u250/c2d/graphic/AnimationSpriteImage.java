52ec8d5cdc89072130069bd69ea6b991954b4d9c
lycying@gmail.com <lycying@gmail.com@5dc86219-f9e5-93d7-e668-893618218886>
2012-07-23 01:35:41 +0000

    animation sprite

--- trunk/c2d/src/info/u250/c2d/graphic/AnimationSpriteImage.java
+++ trunk/c2d/src/info/u250/c2d/graphic/AnimationSpriteImage.java
@@ -3,6 +3,7 @@ package info.u250.c2d.graphic;
 import info.u250.c2d.engine.Engine;
 import info.u250.c2d.graphic.AnimationSprite;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -18,6 +19,9 @@ public class AnimationSpriteImage extends Actor {
 	}
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha) {
+		final Color color = getColor();
+		this.sprite.setOrigin(this.getOriginX(), this.getOriginY());
+		this.sprite.setColor(color);
 		this.sprite.setScale(this.getScaleX(), this.getScaleY());
 		this.sprite.setRotation(this.getRotation());
 		this.sprite.setPosition(this.getX(), this.getY());
