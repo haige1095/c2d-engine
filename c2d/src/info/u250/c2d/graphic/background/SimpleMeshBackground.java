583e3c2d0738237a5ff0a2dd81d6ae5660a31616
lycying <lycying@gmail.com>
2012-11-15 07:29:24 +0000

    add a simple rain effect

--- c2d/src/info/u250/c2d/graphic/background/SimpleMeshBackground.java
+++ c2d/src/info/u250/c2d/graphic/background/SimpleMeshBackground.java
@@ -2,6 +2,7 @@ package info.u250.c2d.graphic.background;
 
 
 import info.u250.c2d.engine.Engine;
+import info.u250.c2d.engine.service.Disposable;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -9,22 +10,23 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 /**
  * this is a simple background for test or publish , 
  * it save a lot space of image just use a fews of codes . 
- * Note it only support OpenglEs 1.x currently .
+ * Works on opengl1.x and 2.x use the default shader .
+ * Do not forgot to dispose it 
  * @author lycying@gmail.com
  *
  */
-public class SimpleMeshBackground {
+public class SimpleMeshBackground implements Disposable{
 	Mesh mesh;
+	ShaderProgram shader;
 	public SimpleMeshBackground(){
 		this(new Color(1, 1, 1, 1f),new Color(152f/255f, 181f/255f, 249f/255f, 1));
 	}
 	public SimpleMeshBackground(Color color1,Color color2){
-		if(Gdx.graphics.isGL20Available()){
-			Gdx.app.error("C2d", "this method only support GL1.x ");
-		}
 		float WIDTH = Engine.getEngineConfig().width;
 		float HEIGHT = Engine.getEngineConfig().height;
 		float[] vertices = new float[] {
@@ -35,16 +37,33 @@ public class SimpleMeshBackground {
 		};
 		short[] indices = new short[] {0, 1, 2, 2, 3, 0};
 		
-		mesh = new Mesh(false, 4, 6, new VertexAttribute(Usage.Position, 2, "a_pos"), new VertexAttribute(Usage.Color, 4, "a_col"));
+		mesh = new Mesh(false, 4, 6, new VertexAttribute(Usage.Position, 2, ShaderProgram.POSITION_ATTRIBUTE), new VertexAttribute(Usage.Color, 4,ShaderProgram.COLOR_ATTRIBUTE));
 		mesh.setVertices(vertices);
 		mesh.setIndices(indices);
+		if(Gdx.graphics.isGL20Available()){
+			shader = SpriteBatch.createDefaultShader();
+		}
 	}
 	
 	
 	public void render (float delta) {
-		GL10 gl = Gdx.gl10;
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		mesh.render(GL10.GL_TRIANGLES);
+		if(Gdx.graphics.isGL20Available()){
+			shader.begin();
+			shader.setUniformMatrix("u_projTrans", Engine.getDefaultCamera().combined);
+			mesh.render(shader, GL10.GL_TRIANGLES);
+			shader.end();
+		}else{
+			GL10 gl = Gdx.gl10;
+			gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+			mesh.render(GL10.GL_TRIANGLES);
+		}
 	}
-	
+	@Override
+	public void dispose() {
+		if(Gdx.graphics.isGL20Available()){
+			if(null!=shader){
+				shader.dispose();
+			}
+		}
+	}
 }
