dd0d46ef025c39f1ed8a69828046bff93b1d800c
lycying <lycying@gmail.com>
2012-12-09 09:37:54 +0000

    add new box2d model .

---
+++ c2d/src/info/u250/c2d/box2d/model/b2Model.java
@@ -0,0 +1,11 @@
+package info.u250.c2d.box2d.model;
+
+
+public abstract class b2Model implements java.io.Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	/**
+	 * This is the  name of the model , every model should have a name 
+	 */
+	public String name = "";
+}
