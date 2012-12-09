3de4900dfa5ce4794b9bce5b5726de92b197380c
lycying <lycying@gmail.com>
2012-12-09 09:34:41 +0000

    remove the old c2d-tools . XXX

--- c2d-tools/src/info/u250/c2d/tools/staff/SceneLayerInputStaff.java
+++
@@ -1,9 +0,0 @@
-package info.u250.c2d.tools.staff;
-
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-
-public abstract class SceneLayerInputStaff extends InputStaff {
-	public SceneLayer layer;
-	public abstract Table layoutInfo();
-	public abstract void buildStaff();
-}
