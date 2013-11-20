e138bff065ac833d59998165af022e2ff2ebc09c
lycying@gmail.com <lycying@gmail.com@5dc86219-f9e5-93d7-e668-893618218886>
2013-11-20 09:16:33 +0000

    if the subfix is pp you will load the particle file with textureatlas

--- trunk/c2d/src/info/u250/c2d/engine/resources/rules/RuleParticleEffect.java
+++ trunk/c2d/src/info/u250/c2d/engine/resources/rules/RuleParticleEffect.java
@@ -3,6 +3,7 @@ package info.u250.c2d.engine.resources.rules;
 import info.u250.c2d.engine.Engine;
 import info.u250.c2d.engine.resources.AliasResourceManager.LoadResourceRule;
 
+import com.badlogic.gdx.assets.loaders.ParticleEffectLoader.ParticleEffectParameter;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 /**
@@ -12,7 +13,17 @@ public class RuleParticleEffect  implements LoadResourceRule{
 	@Override
 	public boolean match(FileHandle file) {
 		boolean result = file.extension().equals("p");
-		if(result) Engine.getAssetManager().load(file.path().replace("\\", "/"),ParticleEffect.class);
-		return  result;
+		if(result) {
+			Engine.getAssetManager().load(file.path().replace("\\", "/"),ParticleEffect.class);
+			return result;
+		}
+		result = file.extension().equals("pp");
+		if(result) {
+			ParticleEffectParameter parameter = new ParticleEffectParameter();
+			parameter.atlasFile = file.pathWithoutExtension().replace("\\", "/")+".atlas";
+			Engine.getAssetManager().load(file.path().replace("\\", "/"),ParticleEffect.class,parameter);
+			return result;
+		}
+		return  false;
 	}
 }
